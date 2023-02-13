68257a6ea5cba688728186198116456695eff8d1
Painless-destiny <1021716293@qq.com>
2023-02-13 22:41:17 +0800

    Initial commit

---
+++ src/main/java/com/first/domain/book.java
@@ -0,0 +1,12 @@
+package com.first.domain;
+
+
+import lombok.Data;
+
+@Data
+public class book {
+    private Integer id;
+    private String Name;
+    private String Country;
+    private String District;
+}
